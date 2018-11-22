a3cc3c13907cb89d756ac0217f67951ec5e716a5
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-11-23 10:36:17 +1100

    basics section completed.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+// objects
+
+let person = {
+    name: "Mosh",
+    age: 30
+};
+
+person["name"] = "Mary"; // reassign property name.
+
+console.log(person.name); // dot notation
+
+console.log(person["name"]); // bracket notation
+
+let selectedProperty = "age";
+
+console.log(person[selectedProperty]); // determine the property selected at runtime/dynamically.
+
+console.log(person.selectedProperty); // cannot use dot notation as "selectedProperty" is not linked to person object.
+
+// arrays
+
+let selectedColors = ["red","blue"];
+
+selectedColors[2] = "green";
+
+console.log(selectedColors.length);
