3a5265d14c7a764e62962b925c1b959eab9ef34a
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-11-24 18:12:10 +1100

    Control Flow Section - Excercise 8 Completed.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-function showProperties(obj) {
+function sum(limit) {
 
-    for (const key in obj) {
-        if (typeof obj[key] === "string") {
-            console.log(key, obj[key]);
-        }
+    const firstMultiple = 3;
+    const secondMultiple = 5;
+    const initialValue = 0
+    let sumMultiples3and5 = initialValue;
+
+    for (let index = initialValue; index <= limit; index++) {
+
+        if (index % firstMultiple === initialValue || index % secondMultiple === initialValue )
+            sumMultiples3and5 += index;
     }
+    
+    return sumMultiples3and5
 }
 
-const movie = {
-
-    title: "a",
-    releaseYear: 2018,
-    rating: 4.5,
-    director: "b",
-    comments: "This is boring."
-};
-
-showProperties(movie);
+console.log(sum(6));
