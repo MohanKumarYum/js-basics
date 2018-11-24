d9ffc302c173346d97858cbf0c9ddff0bb8181aa
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-11-24 17:24:24 +1100

    Control Flow Section - Excercise 5 Completed.

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
-function checkSpeed(input) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-  const maxDemeritPoints = 12;
+showNumbers(11);
 
-  if (typeof input !== "number") return NaN;
+function showNumbers(limit) {
+    for (let index = 0; index <= limit; index++) {
 
-  if (input < speedLimit + kmPerPoint) return "Ok";
+        // if (index % 2 === 0) {
+        //     console.log(index, "Even");
+        // } else
+        //     console.log(index, "Odd");
 
-  if (input > speedLimit) {
-    const demeritPoints = Math.floor((input - speedLimit) / kmPerPoint);
-
-    if (demeritPoints >= maxDemeritPoints) {
-      return "License Suspended";
+        const message = (index % 2 === 0) ? "Even" : "Odd";
+        console.log(index, message);
     }
-
-    return "Points: " + demeritPoints;
-  }
 }
-
-console.log(checkSpeed("Hello"));
-console.log(checkSpeed(50));
-console.log(checkSpeed(70));
-console.log(checkSpeed(75));
-console.log(checkSpeed(76));
-console.log(checkSpeed(80));
-console.log(checkSpeed(84));
-console.log(checkSpeed(96));
-console.log(checkSpeed(125));
-console.log(checkSpeed(130));
-
-
-// Result
-
-// NaN
-// Ok
-// Ok
-// Points: 1
-// Points: 1
-// Points: 2
-// Points: 2
-// Points: 5
-// Points: 11
-// License Suspended
