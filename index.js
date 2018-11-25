e6db2ae411e9bda4a278f1a1edf36f0cdb4eac84
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-11-25 13:31:13 +1100

    Objects Section - Excercise 2 Completed.

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const address = {
-    street: "station",
-    city: "sydney",
-    postcode: 2000
-};
+// const address = {
+//     street: "station",
+//     city: "sydney",
+//     postcode: 2000
+// };
 
 function showAddress(address) {
     for (const key in address)
         console.log(key, address[key]);
 }
+const address = createAddress("station", "sydney", 2000);
 
 showAddress(address);
+
+function createAddress(street, city, postcode) {
+    return {
+        street,
+        city,
+        postcode
+    };
+}
+
+function Address(street, city, postcode) {
+        this.street = street;
+        this.city = city;
+        this.postcode = postcode;
+}
+
+const address1 = new Address("smith", "sydney", 2000);
+
+showAddress(address1);
+
