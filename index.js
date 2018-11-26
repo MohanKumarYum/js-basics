1f3d3fa203c709fec1e6cc197307cab04cc3e8cc
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-11-26 12:35:42 +1100

    Objects Section - Excercise 5 completed.

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const blogObject = {
-  title: "My Blog post",
-  body: "lorem ipsum",
-  author: "Mohan Kumar",
-  views: 2,
-  comments: [
-    {
-      author: "Unknown User",
-      body: "I like this."
-    },
-    {
-      author: "Unknown User2",
-      body: "I like this."
-    }
-  ],
-  isLive: true
-};
+function Post(title, author, body) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views= 0;
+  this.comments= [];
+  this.isLive= false;
+}
 
-console.log(blogObject);
+const post = new Post("My new blog","Mohan Kumar","hello");
+
+console.log(post);
